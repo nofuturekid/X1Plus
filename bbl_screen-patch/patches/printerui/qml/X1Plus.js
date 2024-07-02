351292e224b65a1b61a5a1055d2e2738551cc8f3
jon <149451641+jphannifan@users.noreply.github.com>
2024-07-02 16:15:42 -0700

    QML end of x1plusd "printer service"
    The purpose of /x1plus/Printer.js is to expose QML PrintManager and other properties to x1plusd, and to listen for signals from x1plusd for controlling core printer functions.
    
    Signals from QML to x1plusd:
    - Printer status and current layer: signals generated in onPropertyChanged() in PrintListener.qml
    - PrintOptionsPage.qml settings updates (not implemented)
    
    Signals from x1plusd to QML:
    - Run vibration comp or bed mesh calibration
    - Change printer config (property on PrintOptionsPage.qml)
    - Start/stop/resume a print (not implemented yet)

--- bbl_screen-patch/patches/printerui/qml/X1Plus.js
+++ bbl_screen-patch/patches/printerui/qml/X1Plus.js
@@ -14,6 +14,7 @@
 .import "./x1plus/TempGraph.js" as X1PlusTempGraph
 .import "./x1plus/OTA.js" as X1PlusOTA
 .import "./x1plus/Network.js" as X1PlusNetwork
+.import "./x1plus/Printer.js" as X1PlusPrinter
 
 /* Back-end model logic for X1Plus's UI
  *
@@ -63,6 +64,8 @@ X1Plus.OTA = X1PlusOTA;
 var OTA = X1PlusOTA;
 X1Plus.Network = X1PlusNetwork;
 var Network = X1PlusNetwork;
+X1Plus.Printer = X1PlusPrinter;
+var Printer = X1PlusPrinter;
 
 Stats.X1Plus = X1Plus;
 DDS.X1Plus = X1Plus;
@@ -74,6 +77,7 @@ Settings.X1Plus = X1Plus;
 TempGraph.X1Plus = X1Plus;
 OTA.X1Plus = X1Plus;
 Network.X1Plus = X1Plus;
+Printer.X1Plus = X1Plus;
 
 var _DdsListener = JSDdsListener.DdsListener;
 var _X1PlusNative = JSX1PlusNative.X1PlusNative;
@@ -86,17 +90,6 @@ var printerConfigDir = null;
 
 var emulating = _X1PlusNative.getenv("EMULATION_WORKAROUNDS");
 X1Plus.emulating = emulating;
-
-
-function isIdle() {
-	return PrintManager.currentTask.stage < PrintTask.WORKING;
-}
-X1Plus.isIdle = isIdle;
-
-function hasSleep() {
-	return DeviceManager.power.hasSleep;
-}
-X1Plus.hasSleep = hasSleep;
 
 function loadJson(path) {
 	let xhr = new XMLHttpRequest();
@@ -165,13 +158,17 @@ function awaken(_DeviceManager, _PrintManager, _NetworkManager, _PrintTask, _Net
 	_X1PlusNative.system("mkdir -p " + _X1PlusNative.getenv("EMULATION_WORKAROUNDS") + printerConfigDir);
 	Settings.awaken();
 	OTA.awaken();
+	console.log("1234 loaded OTA")
+	Printer.awaken();
 	BedMeshCalibration.awaken();
 	ShaperCalibration.awaken();
 	GpioKeys.awaken();
 	TempGraph.awaken();
 	Network.awaken();
 	console.log("X1Plus.js is awake");
+	
 }
+
 
 X1Plus.DBus.registerMethod("ping", (param) => {
 	param["pong"] = "from QML";
